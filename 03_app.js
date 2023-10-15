8c49b658fe5dcd9f7989e67186fe054908e77a40
AbhijitPramanick <amanik1995@gmail.com>
2023-10-15 12:29:42 +0530

    Understanding Server making, server method Listen, Nodejs program lifecycle, and request

---
+++ 03_app.js
@@ -0,0 +1,7 @@
+// Node JS Basics
+const http = require("http");
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  //   process.exit();
+});
+server.listen(3000);
