1d2a308ea23f511bc58b0dfe3affce87a0fe4c00
AbhijitPramanick <amanik1995@gmail.com>
2023-10-15 12:40:18 +0530

    Nitty gritty code for how to work with rquest and send responses.

--- 03_app.js
+++ 03_app.js
@@ -3,5 +3,18 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
   //   process.exit();
+  res.setHeader("content-type", "text/html");
+  res.write(`<html>`);
+  res.write(
+    `<head>
+        <title>My first page</title>
+      </head>`
+  );
+  res.write(
+    `<body>
+        <h1> Hello there, this is Node js server.</h1>
+      </body>`
+  );
+  res.write(`</html>`);
 });
 server.listen(3000);
