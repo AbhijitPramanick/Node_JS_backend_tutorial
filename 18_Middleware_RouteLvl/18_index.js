9a794761494bffe6ded30051ced4f07eb2ebbaaa
AbhijitPramanick <amanik1995@gmail.com>
2023-10-19 10:01:23 +0530

    Understanding Route level middleware

--- 18_Middleware_RouteLvl/18_index.js
+++ 18_Middleware_RouteLvl/18_index.js
@@ -1,32 +1,30 @@
+// Adding middleware to each and every route can lead to repetitive code and make your codebase harder to maintain. To address this issue, you can create a route instance with the middleware applied to it. This approach is often referred to as "route grouping" or "route middleware."
 const express = require("express");
-
 const app = express();
 
+//Importing the middleware function
 const reqFilter = require(`./middleware`);
-// Middleware
 
-// const reqFilter = (req, resp, next) => {
-//   if (!req.query.age) {
-//     resp.send("<h1>Provide age.</h1>");
-//   } else if (req.query.age < 18) {
-//     resp.send(`<h1>You are not allowed to access this page.</h1>`);
-//   } else {
-//     next();
-//   }
-// };
-
-//Following was the application level middleware, where as Route-level middleware can be applied to spcific routes, group of routes or the every route.
-
-// app.use(reqFilter);
-
-//making seperate files of middleware is a good practice, beacuse middlewares can be more than 1 , making seperate files for each helps
+//// Create a route instance with middleware applied
+const route = express.Router();
+route.use(reqFilter);
 
 app.set("view engine", "ejs");
 
 app.get("/profile", (_, resp) => {
   resp.render("profile");
 });
-app.get("/user", reqFilter, (_, resp) => {
+route.get("/user", (_, resp) => {
   resp.render("user");
 });
+app.get("/contact", (_, resp) => {
+  resp.send(`<h1>This is contact page.</h1>`);
+});
+route.get("/help", (_, resp) => {
+  resp.send(`<h1>This is help page.</h1>`);
+});
+
+// Mount the route instance at a specific URL
+app.use("/", route);
+
 app.listen(3200);
